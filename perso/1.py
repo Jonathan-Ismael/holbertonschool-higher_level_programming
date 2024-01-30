294046ccbe2c875586cd0614603d9f1bc40d01c5
root <ismazpek@gmail.com>
2024-01-30 05:42:14 -0800

    1.py

---
+++ perso/1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    liste_1 = [1, 4, 2, 7, 35, 84]
+    villes = ['Paris', 'Berlin', 'Londres', 'Bruxelles']
+    liste_2 = [liste_1, villes]
+    liste3 = []
