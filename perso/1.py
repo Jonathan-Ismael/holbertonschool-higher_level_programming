0a902ab7878bc9b3f2c97c52836d314748f8422f
root <ismazpek@gmail.com>
2024-01-30 05:42:49 -0800

    1.py

--- perso/1.py
+++ perso/1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    
     liste_1 = [1, 4, 2, 7, 35, 84]
     villes = ['Paris', 'Berlin', 'Londres', 'Bruxelles']
     liste_2 = [liste_1, villes]
-    liste3 = []
+    liste3 =[]
+
+    tuple_1 = (1, 2, 6, 1, 7)
+
+    prenom = 'Nicolas'
+
+    print(villes[0])
+    print(villes[1])
+    print('{}'.format(villes[-1]))
