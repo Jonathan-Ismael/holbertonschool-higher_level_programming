56ff38ad69cd01c28beb137116206dce15b0f8c8
root <ismazpek@gmail.com>
2024-02-01 01:51:08 -0800

    isok

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    num = {'I': 1, 'V': 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    prev_value = 0
+    if isinstance(roman_string, str) is True:
+        for i in reversed(roman_string):
+            value = num.get(i, 0)
+            if value >= prev_value:
+                result += value
+            else:
+                result -= value
+            prev_value = value
+        return result
+    else:
+        return 0
